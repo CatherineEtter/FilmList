0b8447757be14740c98dc5ae56e5e094bf701ced
Colin S <fictiondude@gmail.com>
2019-03-02 13:28:38 -0600

    pull user doc associated with my email (fictiondude) from firestore

--- src/catalog.js
+++ src/catalog.js
@@ -3,3 +3,20 @@ function testAuth(){
         console.log("user id: " + firebase.auth().currentUser.uid);
     }
 }
+
+function getCatalog(){
+    var db = firebase.firestore();
+    var users = db.collection("users");
+    users.doc(""+firebase.auth().currentUser.uid).get().then(function(doc) {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+
+
+}
