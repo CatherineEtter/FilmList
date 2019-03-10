4fc312c89a107dde47e65cd2589f7bf58a07d1ca
rogin <rogin@users.noreply.github.com>
2019-03-10 14:31:23 -0500

    merge master

--- src/login.js
+++ src/login.js
@@ -1,14 +1,3 @@
- // Initialize Firebase
-  var config = {
-     apiKey: "Hidden",
-     authDomain: "movieproject-9d83d.firebaseapp.com",
-     databaseURL: "https://movieproject-9d83d.firebaseio.com",
-     projectId: "movieproject-9d83d",
-     storageBucket: "movieproject-9d83d.appspot.com",
-     messagingSenderId: "721138716237"
-   };
-firebase.initializeApp(config);
-
 function loginUser() {
     var email = document.getElementById("emailInput").value
     var password = document.getElementById("passwordInput").value
