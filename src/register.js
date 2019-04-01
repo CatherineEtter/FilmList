d52d831ef56226cedd05b26ac9c52a4a9dcd18e4
Richard Ogin <rogin@users.noreply.github.com>
2019-03-31 20:49:20 -0400

    enable toggling of search details

--- src/register.js
+++ src/register.js
@@ -1,29 +1,19 @@
 function RegisterForm(){
-    var password1;
-    var password2;
-    var email;
-    email = document.getElementById("user_field").value;
-    password1 = document.getElementById("pass_field1").value;
-    password2 = document.getElementById("pass_field2").value;
+    var email = document.getElementById("user_field").value;
+    var initialPassword = document.getElementById("pass_field1").value;
+    var confirmationPassword = document.getElementById("pass_field2").value;
 
-    if (password1 == password2) {
-        
-    firebase.auth().createUserWithEmailAndPassword(email, password1).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-        });    
-        window.alert("The create user works!!")
-    }
-    
-    
-    
+    if (initialPassword == confirmationPassword) {
+        firebase.auth().createUserWithEmailAndPassword(email, initialPassword).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+            });    
+            window.alert("The create user works!!")
+        }
     else  {
-        
-    window.alert("Passwords Do Not Match, Try Again.")
-        
-        
+        window.alert("Passwords Do Not Match, Try Again.")
     }
 }
 
