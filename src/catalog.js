87ac01bbc822be92db2d37b3cc31e269734577dd
Colin S <fictiondude@gmail.com>
2019-04-13 20:14:47 -0500

    Back to 2 collections. Just deal with data dupe??

--- src/catalog.js
+++ src/catalog.js
@@ -16,28 +16,46 @@ function testAuth(){
     }
 }
 
+function testGetCatalog(){
+    var catalogQ = getCatalog();
+
+    var test = arrayify(catalogQ);
+
+    consoleOutputPromiseArray(test);
+
+    //console.log("Outputting a filtered query. ");
+    //consoleOutputPromiseArray(arrayify(filterYear(catalogQ, "1938")));
+    
+}
+
 function getCatalog(){
     var db = firebase.firestore();
     var users = db.collection("users");
     var collectionId = "users/"+firebase.auth().currentUser.uid+"/movies";
     var movies = db.collection(collectionId);
-    var test = movies.where("state", "<=", 1).get()
-        .then(function(snapshot){
-            var ary = [];
-            snapshot.forEach(function(doc){
-                ary.push(doc.data());
-            });
-            return ary;
-        }).catch(function(error){
-            console.log("Error getting doc: ", error);
-        });
-    console.log(test);
+    return movies.where("state", "<=", 1).where("year", "==", "1938");
+}
 
-    test.then(function(ary){
+function filterYear(query, year){
+    return query.where("year", "==", year);
+}
+
+function arrayify(query){
+    return query.get().then(function(snapshot){
+        var ary = [];
+        snapshot.forEach(function(doc){
+            ary.push(doc.data());
+        });
+        return ary;
+    }).catch(function(error){
+        console.log("Error getting doc: ", error);
+    });
+}
+
+function consoleOutputPromiseArray(promiseArray){
+    promiseArray.then(function(ary){
         for(var i=0; i < ary.length; i++){
             console.log(ary[i]);
         }
     });
-
-    return test;
 }
