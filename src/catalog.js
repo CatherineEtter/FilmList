3ace818d59b926c46f4a325d1bdfcc1f015856d4
Colin S <fictiondude@gmail.com>
2019-04-21 23:29:28 -0500

    Filtering is working!!

--- src/catalog.js
+++ src/catalog.js
@@ -25,35 +25,84 @@ function titleSearch(){
     }
 }
 
-function searchCatalog(){
+function additiveSearchCatalog(){
     
     var CID = catalogCID();
     var actorInput = $("#actor-input").val().trim();
     var directorInput = $("#director-input").val().trim();
     var genreInput = $("#genre-select").val();
-    console.log(genreInput.length);
+
+    var actorArray;
+    var directorArray;
+    var genreArray;
+
+    var arrayArray = [];
+    var finalArray = [];
     
     if(actorInput !== '') {
         console.log("filtering for actor = " + actorInput);
-        CID = CID.where("Actors", "array-contains", actorInput);
+        actorArray = arrayify(CID.where("Actors", "array-contains", actorInput));
+        console.log(actorArray);
+        arrayArray.push(actorArray);
     }
 
     if(directorInput !== '') {
         console.log("filtering for director = " + directorInput);
-        CID = CID.where("Director", "==", directorInput);
+        directorArray = filterDirector(CID, directorInput);
+        arrayArray.push(directorArray);
     }
 
     if(genreInput.length!=0){
-        var ary1 = filterGenre(CID, genreInput[0]);
+        genreArray = filterGenre(CID, genreInput[0]);
         for(var i=1; i<genreInput.length; i++){
             console.log("merging arrays");
-            ary1 = mergePromiseArrays(filterGenre(CID, genreInput[i]), ary1);
+            genreArray = mergePromiseArrays(filterGenre(CID, genreInput[i]), genreArray);
         }
+        console.log(genreArray);
+        arrayArray.push(genreArray);
     }
+
+    //consoleOutputPromiseArray(actorArray);
+    //consoleOutputPromiseArray(genreArray);
+    consoleOutputPromiseArray(intersectPromiseArrays(actorArray,genreArray));
+
 
     //CID = CID.orderBy("TimeAdded", "asc");
     //consoleOutputPromiseArray(arrayify(CID));
-    consoleOutputPromiseArray(ary1);
+    //consoleOutputPromiseArray(ary1);
+}
+
+function exclusiveSearchCatalog(){
+    var CID = catalogCID();
+    var actorInput = $("#actor-input").val().trim();
+    var directorInput = $("#director-input").val().trim();
+    var genreInput = $("#genre-select").val();
+    var ratingInput = $("#rating-input").val().trim();
+    var completeArray = getWholeCatalog(CID, "asc").then(function(array){
+        return array.filter(function(value){
+            console.log("Current Value: " + value);
+            var result = true;
+            if(actorInput != ''){
+                result = result && value["Actors"].includes(actorInput);
+            }
+            if(genreInput.length != 0){
+                for(var i=0; i<genreInput.length; i++){
+                    console.log(genreInput[i] + " " + result && value["Genre"].includes(genreInput[i]));
+                    result = result && value["Genre"].includes(genreInput[i]);
+                }
+            }
+            if(directorInput != ''){
+                result = result && value["Director"] == directorInput;
+            }
+            if(ratingInput != ''){
+                result = result && value["imdbRating"] >= ratingInput;
+            }
+            return result;
+        });
+    });
+    displayCatalog(completeArray);
+    consoleOutputPromiseArray(completeArray);
+
 }
 
 function testGetCatalog(){
@@ -95,7 +144,11 @@ function filterGenre(CID, genre){
 }
 
 function filterActor(CID, actor){
-    return arrayify(CID.where("Actor", "array-contains", actor));
+    return arrayify(CID.where("Actors", "array-contains", actor));
+}
+
+function filterDirector(CID, director){
+    return arrayify(CID.where("Director", "==", director));
 }
 
 function arrayify(query){
@@ -120,6 +173,48 @@ function mergePromiseArrays(arrayOne, arrayTwo){
     });
 }
 
+function intersectPromiseArraysTwo(ary1, ary2){
+    return ary1.then(function(array1){
+        return ary2.then(function(array2){
+            var newArray = array2.filter(value => array1.includes(value));
+            console.log(newArray);
+            return newArray;
+        });
+    });
+}
+
+
+function intersectPromiseArrays(arrayOne, arrayTwo){
+    console.log(arrayOne);
+    return arrayOne.then(function(ary1){
+        return arrayTwo.then(function(ary2){
+            //console.log(ary1);
+            //return ary1.concat(ary2);
+
+            var ary1Index = ary2Index= 0;
+            var result = [];
+
+            while( ary1Index < arrayOne.length && ary2Index < arrayTwo.length ){
+                if(ary1[ary1Index] < ary2[ary2Index] ){
+                    ary1Index++;
+                }
+                else if (ary1[ary1Index] > ary2[ary2Index] )
+                {
+                    ary2Index++;
+                }
+                else /* they're equal */
+                {
+                    result.push(ary1[ary1Index]);
+                    ary1Index++;
+                    ary2Index++;
+                }
+            }
+
+            return result;
+        });
+    });
+}
+
 function consoleOutputPromiseArray(promiseArray){
     promiseArray.then(function(ary){
         for(var i=0; i < ary.length; i++){
@@ -133,28 +228,30 @@ function displayFilteredGenreCatalog(genreType) {
 function displayFilteredYearCatalog(year) {
     
 }
+
 function displayUnfilteredCatalog() {
     var catalogPromise = getWholeCatalog(catalogCID(),'asc');
-    catalogPromise.then(function(array) {
-        displayCatalog(array);
-    });
+    displayCatalog(catalogPromise);
 }
 //TODO make functions for each filter to get the filtered catalog and display it
 //takes the catalog of movie information and displays them to the user in a list
-function displayCatalog(catArray) {
+function displayCatalog(catalogPromiseArray) {
     //console.log(movielist);
-    console.log(catArray);
-    var listBuilder;
-    for(var i = 0; i < catArray.length; i++) {
-        for(var key in catArray[i]) {
-            //listBuilder = (' <div class="squareImage" style="background-image: url(\'' + catArray[i]['Poster'] + ' \');"> ');
-            listBuilder = (' <div class="square-content-container" style="background: #282828"> ');
-            listBuilder += (' <p> ' + catArray[i]['Title']+ ' </p>');
-            listBuilder += (' <img src=" ' + catArray[i]['Poster'] + ' "/> ');
-            listBuilder += (' <p> ' + catArray[i]['Year']+ ' </p>');
-            listBuilder += (' </div> ');
+    //console.log(catArray);
+    $( "#movie-listing-container" ).empty();
+    catalogPromiseArray.then(function(catArray) {
+        var listBuilder;
+        for(var i = 0; i < catArray.length; i++) {
+            for(var key in catArray[i]) {
+                //listBuilder = (' <div class="squareImage" style="background-image: url(\'' + catArray[i]['Poster'] + ' \');"> ');
+                listBuilder = (' <div class="square-content-container" style="background: #282828"> ');
+                listBuilder += (' <p> ' + catArray[i]['Title']+ ' </p>');
+                listBuilder += (' <img src=" ' + catArray[i]['Poster'] + ' "/> ');
+                listBuilder += (' <p> ' + catArray[i]['Year']+ ' </p>');
+                listBuilder += (' </div> ');
+            }
+            console.log(listBuilder);
+            $( "#movie-listing-container" ).append(listBuilder);
         }
-        console.log(listBuilder);
-        $( "#movie-listing-container" ).append(listBuilder);
-    }
+    });
 }
